67bf0b0410d7f64c9798772f09583317085f85b9
Yasmim Graciano <yasmimgracianosfx@gmail.com>
2025-01-28 11:20:42 -0300

    Adicionando novos conversores

--- conversor.py
+++ conversor.py
@@ -15,3 +15,19 @@ def kelvin_para_celsius(kelvin: float) -> float:
     celsius = kelvin - 273.15
     return celsius
 
+
+def celsius_para_fahrenheit(celsius: float) -> float:
+    """
+    Transforma uma temperatura de Celsius para Fahrenheit
+    """
+    fahrenheit = (celsius * 9 / 5) + 32
+    return fahrenheit
+
+
+def kelvin_para_fahrenheit(kelvin: float) -> float:
+    """
+    Transforma uma temperatura de Kelvin para Fahrenheit
+    """
+    celsius = kelvin_para_celsius(kelvin)
+    fahrenheit = celsius_para_fahrenheit(celsius)
+    return fahrenheit
